0bd1a838c6645cfd3eab85c864e495cfcb2fee8a
rodrigojmcosta <rodrigojosemodesto@gmail.com>
2022-07-12 17:06:14 -0300

    User class created

--- src/main/java/br/com/learning/junitlearning/JunitLearningApplication.java
+++ src/main/java/br/com/learning/junitlearning/JunitLearningApplication.java
@@ -1,5 +1,6 @@
 package br.com.learning.junitlearning;
 
+import br.com.learning.junitlearning.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
