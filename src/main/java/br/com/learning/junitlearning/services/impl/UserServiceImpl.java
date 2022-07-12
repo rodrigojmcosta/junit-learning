a3c8b463f47268367dd02760243444653e949f4f
rodrigojmcosta <rodrigojosemodesto@gmail.com>
2022-07-12 19:28:06 -0300

    ObjectNotFound exception handling

--- src/main/java/br/com/learning/junitlearning/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/learning/junitlearning/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.learning.junitlearning.services.impl;
 import br.com.learning.junitlearning.domain.User;
 import br.com.learning.junitlearning.repositories.UserRepository;
 import br.com.learning.junitlearning.services.UserService;
+import br.com.learning.junitlearning.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() ->new ObjectNotFoundException("Objeto não encontrado"));
     }
 
 }
