688d415fb79675122f379c9b7a88368569172a7d
rodrigojmcosta <rodrigojosemodesto@gmail.com>
2022-07-12 16:52:23 -0300

    Project initialized

---
+++ src/main/java/br/com/learning/junitlearning/JunitLearningApplication.java
@@ -0,0 +1,13 @@
+package br.com.learning.junitlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/learning/junitlearning/JunitLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.learning.junitlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
