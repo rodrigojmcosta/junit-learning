72a4f7fbfbc70ee826cb518a0e431c984c19ce33
rodrigojmcosta <rodrigojosemodesto@gmail.com>
2022-07-12 23:36:56 -0300

    User DELETE endpoint created

--- src/main/java/br/com/learning/junitlearning/resources/UserResource.java
+++ src/main/java/br/com/learning/junitlearning/resources/UserResource.java
@@ -1,6 +1,5 @@
 package br.com.learning.junitlearning.resources;
 
-import br.com.learning.junitlearning.domain.User;
 import br.com.learning.junitlearning.domain.dto.UserDTO;
 import br.com.learning.junitlearning.services.UserService;
 import org.modelmapper.ModelMapper;
@@ -11,11 +10,12 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
+
+    public static final String ID = "/{id}";
 
     @Autowired
     private ModelMapper mapper;
@@ -23,7 +23,7 @@ public class UserResource {
     @Autowired
     private UserService service;
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = ID)
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
         return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
@@ -37,15 +37,20 @@ public class UserResource {
     @PostMapping
     public ResponseEntity<UserDTO> create(@RequestBody UserDTO userDTO) {
         URI uri = ServletUriComponentsBuilder
-                .fromCurrentRequest().path("/{id}").buildAndExpand(service.create(userDTO).getId()).toUri();
+                .fromCurrentRequest().path(ID).buildAndExpand(service.create(userDTO).getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
 
-    @PutMapping(value = "/{id}")
+    @PutMapping(value = ID)
     public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO userDTO) {
         userDTO.setId(id);
         return ResponseEntity.ok().body(mapper.map(service.update(userDTO), UserDTO.class));
     }
 
+    @DeleteMapping(value = ID)
+    public ResponseEntity<UserDTO> delete(@PathVariable Integer id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- src/main/java/br/com/learning/junitlearning/services/UserService.java
+++ src/main/java/br/com/learning/junitlearning/services/UserService.java
@@ -15,4 +15,6 @@ public interface UserService {
 
     User update(UserDTO userDTO);
 
+    void delete(Integer id);
+
 }
