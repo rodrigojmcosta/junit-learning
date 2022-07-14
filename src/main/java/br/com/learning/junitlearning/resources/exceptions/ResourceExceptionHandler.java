7a28544a833aa8309eefde07b9fdc0790f71ae99
rodrigojmcosta <rodrigojosemodesto@gmail.com>
2022-07-14 00:52:55 -0300

    ResourceExceptionHandler DataIntegrityViolationException test implemented

--- src/main/java/br/com/learning/junitlearning/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/learning/junitlearning/resources/exceptions/ResourceExceptionHandler.java
@@ -1,6 +1,6 @@
 package br.com.learning.junitlearning.resources.exceptions;
 
-import br.com.learning.junitlearning.services.exceptions.DataIntegratyViolationException;
+import br.com.learning.junitlearning.services.exceptions.DataIntegrityViolationException;
 import br.com.learning.junitlearning.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,21 +8,20 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.xml.crypto.Data;
 import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException ex, HttpServletRequest request) {
+    public ResponseEntity<StandardError> dataIntegrityViolationException(ObjectNotFoundException ex, HttpServletRequest request) {
         StandardError error =
                 new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
-    @ExceptionHandler(DataIntegratyViolationException.class)
-    public ResponseEntity<StandardError> objectNotFound(DataIntegratyViolationException ex, HttpServletRequest request) {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request) {
         StandardError error =
                 new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
--- src/test/java/br/com/learning/junitlearning/resources/exceptions/ResourceExceptionHandlerTest.java
+++ src/test/java/br/com/learning/junitlearning/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package br.com.learning.junitlearning.resources.exceptions;
 
+import br.com.learning.junitlearning.services.exceptions.DataIntegrityViolationException;
 import br.com.learning.junitlearning.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.test.web.servlet.MockHttpServletRequestDsl;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ResourceExceptionHandlerTest {
 
     public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
+    public static final String EMAIL_JA_CADASTRADO = "Email já cadastrado";
     @InjectMocks
     private ResourceExceptionHandler exceptionHandler;
 
@@ -28,7 +31,25 @@ class ResourceExceptionHandlerTest {
     @Test
     void whenObjectNotFoundExceptionThenReturnAResponseEntity() {
         ResponseEntity<StandardError> response = exceptionHandler
-                .objectNotFound(
+                .dataIntegrityViolationException(
+                        new DataIntegrityViolationException(EMAIL_JA_CADASTRADO),
+                        new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(EMAIL_JA_CADASTRADO, response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
+        assertNotEquals("/user/2", response.getBody().getPath());
+        assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
+    }
+
+    @Test
+    void dataIntegrityViolationException() {
+        ResponseEntity<StandardError> response = exceptionHandler
+                .dataIntegrityViolationException(
                         new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO),
                         new MockHttpServletRequest());
 
@@ -39,10 +60,6 @@ class ResourceExceptionHandlerTest {
         assertEquals(StandardError.class, response.getBody().getClass());
         assertEquals(OBJETO_NAO_ENCONTRADO, response.getBody().getError());
         assertEquals(404, response.getBody().getStatus());
-    }
-
-    @Test
-    void testObjectNotFound() {
     }
 
 
